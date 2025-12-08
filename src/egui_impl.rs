26d636a2796ccfd8d2cde2f8f8ff733dc0884dac
Jari O. O. Pennanen <ciantic@oksidi.com>
2025-12-08 18:40:05 +0200

    wgpu

--- src/egui_impl.rs
+++ src/egui_impl.rs
@@ -1,22 +1,53 @@
+use smithay_client_toolkit::shell::xdg::window::{Window, WindowConfigure};
 use wayland_client::protocol::wl_surface::WlSurface;
 
-struct EguiSurface {
-    pub wl_surface: WlSurface,
+use crate::{BaseTrait, CompositorHandlerContainer, KeyboardHandlerContainer, PointerHandlerContainer, WindowContainer};
 
+struct EguiWgpuSurface {
+    // TODO: Other stuff?
+    pub wl_surface: WlSurface,
+}
+
+impl EguiWgpuSurface {
+    pub fn new(wl_surface: WlSurface) -> Self {
+        // TODO: Other stuff?
+        Self { wl_surface }
+    }
+
+    pub fn configure(&mut self, width: u32, height: u32) {
+    }
 }
 
 struct EguiWindow {
-    pub surface: EguiSurface,
+    pub surface: EguiWgpuSurface,
 }
 
 struct EguiLayerSurface {
-    pub surface: EguiSurface,
+    pub surface: EguiWgpuSurface,
 }
 
 struct EguiPopup {
-    pub surface: EguiSurface,
+    pub surface: EguiWgpuSurface,
 }
 
 struct EguiSubsurface {
-    pub surface: EguiSurface,
+    pub surface: EguiWgpuSurface,
+}
+
+
+impl CompositorHandlerContainer for EguiWindow {}
+impl KeyboardHandlerContainer for EguiWindow {}
+impl PointerHandlerContainer for EguiWindow {}
+impl BaseTrait for EguiWindow {}
+impl WindowContainer for EguiWindow {
+    fn configure(
+        &mut self,
+        configure: &WindowConfigure,
+    ) {
+        todo!()
+    }
+
+    fn get_window(&self) -> &Window {
+        todo!()
+    }
 }
